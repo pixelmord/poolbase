91dc0ddd16140927c34b692f050d584ec10d9f7c
Andreas Adam <andreas.sahle@gmail.com>
2020-09-08 10:47:00 +0200

    chore: debug and emulator config

--- packages/app/lib/initFirebase.ts
+++ packages/app/lib/initFirebase.ts
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/functions';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
@@ -13,23 +13,30 @@ const config = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 const app = !firebase.apps.length ? firebase.initializeApp(config) : firebase.apps[0];
-const firestore = firebase.firestore(app);
+const firestore = firebase.firestore();
+let functions;
+console.log(process.env.NODE_ENV);
 if (
   typeof location !== 'undefined' &&
   location.hostname &&
   location.hostname === 'localhost' &&
-  process.env.NODE_ENV === 'test'
+  process.env.NODE_ENV === 'development'
 ) {
+console.log(process.env.NODE_ENV, location);
   firestore.settings({
     host: 'localhost:8080',
     ssl: false,
   });
-  firebase.functions().useFunctionsEmulator('http://localhost:5001');
+  firebase.functions().useFunctionsEmulator('http://localhost:5005');
+  functions = firebase.app().functions();
+} else {
+  functions = firebase.app().functions('europe-west1');
 }
 
 const auth = firebase.auth(app);
-const functions = firebase.app().functions('europe-west1');
+
 
 export { app, auth, firestore, functions };
 
