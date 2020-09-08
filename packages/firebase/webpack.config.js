dd26791a97ba8cea3450aefa49549b2d277aa622
Andreas Adam <andreas.sahle@gmail.com>
2020-09-08 08:38:29 +0200

    chore: firebase config

--- packages/firebase/webpack.config.js
+++ packages/firebase/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, '../../dist/functions'),
+    path: path.resolve(__dirname, './dist'),
     libraryTarget: 'commonjs',
   },
   plugins: [
