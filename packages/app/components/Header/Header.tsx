33050584ca4298a3299b9ddc0f3808f55101cab4
Andreas Adam <andreas.sahle@gmail.com>
2020-09-23 15:36:24 +0200

    chore: add header buttons for home, language

--- packages/app/components/Header/Header.tsx
+++ packages/app/components/Header/Header.tsx
@@ -1,24 +1,40 @@
 import * as React from 'react';
 import { useColorMode } from '@chakra-ui/core';
 import Router from 'next/router';
-import { PlusSquare as Add, Moon, Sun, LogOut } from 'react-feather';
+import { PlusSquare as Add, Moon, Sun, LogOut, Home, Globe } from 'react-feather';
 
 import AppBar from 'components/AppBar';
 import NavIconButton from 'components/NavIconButton';
 import UserSettingsMenu from 'components/UserSettingsMenu';
-import { useSession } from 'hooks/user';
+import { useSession, useI18n } from 'hooks';
 
 export default function Header(): JSX.Element {
   const { colorMode, toggleColorMode } = useColorMode();
   const { logout } = useSession();
+  const { activeLocale } = useI18n();
+  const alternativeLocale = activeLocale === 'en' ? 'de' : 'en';
   return (
     <AppBar as="header">
       <NavIconButton
+        aria-label="Homepage"
+        onClick={(): void => {
+          Router.push(`/${activeLocale}`);
+        }}
+        icon={Home}
+      ></NavIconButton>
+      <NavIconButton
         aria-label="Add URL"
         onClick={(): void => {
-          Router.push('/en/add-url');
+          Router.push(`/${activeLocale}/add-url`);
         }}
         icon={Add}
+      ></NavIconButton>
+      <NavIconButton
+        aria-label="Switch Language"
+        onClick={(): void => {
+          Router.push(`/${alternativeLocale}`);
+        }}
+        icon={Globe}
       ></NavIconButton>
       <NavIconButton
         aria-label="Logout"
